77e9d397e40e1023a4b697bff305dbbd9f4789d0
sofiane rahmoun <sofianed10@hotmail.fr>
2018-12-29 19:19:13 +0000

    first commit

---
+++ src/main/java/com/example/ApplicationStock/ApplicationStockApplication.java
@@ -0,0 +1,14 @@
+package com.example.ApplicationStock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationStockApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/ApplicationStock/ApplicationStockApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.ApplicationStock;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationStockApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
